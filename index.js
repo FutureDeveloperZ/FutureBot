02d84afe66057bfe7d30b4d2c532ae04375dc22e
Wolfkid200444 <32025746+Wolfkid200444@users.noreply.github.com>
2018-05-14 09:38:02 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const botconfig = require("./botconfig.json");
-
-const Discord = require("discord.js");
-
-const bot = new Discord.Client({disableEveryone: true});
+const Discord = required ("discors.js");
+const bot = new Discord.Client();
 
 bot.on("ready", async () => {
   console.log(`${bot.user.username} is online!`);
